b7722c48a6810c6fe9063850fcb4b93873c9b9e1
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-09 17:53:19 +0900

    Setup to build kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(asm)]
+#![feature(llvm_asm)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // disable name mangling
+pub extern "C" fn KernelMain() -> ! {
+    loop {
+        unsafe { asm!("hlt") }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe { asm!("hlt") }
+    }
 }
