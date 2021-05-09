b589d3ff9d5c7ddd1b886cc3c7689756f7cab994
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-09 19:04:04 +0900

    Implement day03c with rust

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@
 use core::panic::PanicInfo;
 
 #[no_mangle] // disable name mangling
-pub extern "C" fn KernelMain() -> ! {
+pub extern "C" fn KernelMain(frame_buffer_base: u64, frame_buffer_size: u64) -> ! {
+    let frame_buffer = frame_buffer_base as *mut u8;
+    for i in 0..frame_buffer_size {
+        unsafe {
+            *frame_buffer.offset(i as isize) = (i % 256) as u8;
+        }
+    }
+
     loop {
         unsafe { asm!("hlt") }
     }
