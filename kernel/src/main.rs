617fd1c86a83ea8d84756bbfe1c7e34982e59bd8
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-14 23:50:32 +0900

    day05a in rust (#10)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,9 +5,7 @@
 
 mod pixel_writer;
 
-use crate::pixel_writer::{
-    write_pixel, BGRResv8BitPerColorPixelWriter, RGBResv8BitPerColorPixelWriter,
-};
+use crate::pixel_writer::{write_pixel, BGRResv8BitPerColorPixelWriter, RGBResv8BitPerColorPixelWriter, write_ascii, PixelColor};
 use core::panic::PanicInfo;
 use shared::{FrameBufferConfig, PixelFormat};
 
@@ -17,10 +15,16 @@ pub extern "C" fn KernelMain(frame_buffer_config: &FrameBufferConfig) -> ! {
         PixelFormat::KPixelRGBResv8BitPerColor => {
             let writer = RGBResv8BitPerColorPixelWriter::new(frame_buffer_config);
             write_pixel(&writer, frame_buffer_config);
+            let black = PixelColor::new(0, 0, 0);
+            write_ascii(&writer, 50, 50, 'A', &black);
+            write_ascii(&writer, 58, 50, 'A', &black);
         }
         PixelFormat::KPixelBGRResv8BitPerColor => {
             let writer = BGRResv8BitPerColorPixelWriter::new(frame_buffer_config);
             write_pixel(&writer, frame_buffer_config);
+            let black = PixelColor::new(0, 0, 0);
+            write_ascii(&writer, 50, 50, 'A', &black);
+            write_ascii(&writer, 58, 50, 'A', &black);
         }
     };
 
