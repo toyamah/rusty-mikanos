425fb03d175c7d0c4673e676a11a047fe1a21684
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-17 01:47:12 +0900

    Put out constants

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -1,5 +1,8 @@
 use crate::graphics::{PixelColor, PixelWriter};
 
+const ROWS: usize = 25;
+const COLUMNS: usize = 80;
+
 pub struct Console<'a> {
     writer: &'a PixelWriter<'a>,
     fg_color: PixelColor,
@@ -7,13 +10,10 @@ pub struct Console<'a> {
     cursor_row: usize,
     cursor_column: usize,
     // 1 means null character to be written at end of a line
-    buffer: [[char; Console::COLUMNS + 1]; Console::ROWS],
+    buffer: [[char; COLUMNS + 1]; ROWS],
 }
 
 impl<'a> Console<'a> {
-    const ROWS: usize = 25;
-    const COLUMNS: usize = 80;
-
     pub fn new(
         writer: &'a PixelWriter<'a>,
         fg_color: PixelColor,
@@ -25,7 +25,7 @@ impl<'a> Console<'a> {
             bg_color,
             cursor_row: 0,
             cursor_column: 0,
-            buffer: [[char::from(0); Console::COLUMNS + 1]; Console::ROWS],
+            buffer: [[char::from(0); COLUMNS + 1]; ROWS],
         }
     }
 
@@ -33,7 +33,7 @@ impl<'a> Console<'a> {
         for char in str.chars() {
             if char == '\n' {
                 self.new_line();
-            } else if self.cursor_column < Console::COLUMNS - 1 {
+            } else if self.cursor_column < COLUMNS - 1 {
                 self.writer.write_ascii(
                     (8 * self.cursor_column) as u32,
                     (16 * self.cursor_row) as u32,
@@ -48,24 +48,24 @@ impl<'a> Console<'a> {
 
     fn new_line(&mut self) {
         self.cursor_column = 0;
-        if self.cursor_row < Console::ROWS- 1 {
+        if self.cursor_row < ROWS - 1 {
             self.cursor_row += 1;
             return;
         }
 
-        for y in 0..16 * Console::ROWS {
-            for x in 0..8 * Console::COLUMNS {
+        for y in 0..16 * ROWS {
+            for x in 0..8 * COLUMNS {
                 self.writer.write(x as u32, y as u32, &self.bg_color);
             }
         }
 
-        for row in 0..Console::ROWS - 1 {
+        for row in 0..ROWS - 1 {
             let next = row + 1;
             self.buffer.copy_within(next..=next, row);
             self.writer
                 .write_chars(0, (16 * row) as u32, &self.buffer[row], &self.fg_color);
         }
 
-        self.buffer[Console::ROWS - 1].fill(char::from(0));
+        self.buffer[ROWS - 1].fill(char::from(0));
     }
 }
