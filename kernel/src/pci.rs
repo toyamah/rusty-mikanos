d29af6a83bb535c83948724b5bc41d1abad6d58e
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-23 02:03:34 +0900

    Add functions handling pci register

--- kernel/src/pci.rs
+++ kernel/src/pci.rs
@@ -15,3 +15,22 @@ fn make_address(bus: u8, device: u8, function: u8, reg_addr: u8) -> u32 {
     let enabled_bit: u32 = (1 as u32) << 32;
     enabled_bit | shl(bus, 16) | shl(device, 11) | shl(function, 8) | (reg_addr & 0xfc) as u32
 }
+
+fn write_address(address: u32) {
+    unsafe { IoOut32(CONFIG_ADDRESS, address) }
+}
+
+fn write_data(value: u32) {
+    unsafe {
+        IoOut32(CONFIG_DATA, value);
+    }
+}
+
+fn read_data() -> u32 {
+    unsafe { IoIn32(CONFIG_DATA) }
+}
+
+fn read_vendor_id(bus: u8, device: u8, function: u8) -> u32 {
+    write_address(make_address(bus, device, function, 0x00));
+    read_data() & 0xffff
+}
