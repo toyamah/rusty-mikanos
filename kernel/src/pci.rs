1d1b4bab3cdf239d337c531012e25dcae8e300d2
Haruyoshi Toyama <duane0728@gmail.com>
2021-05-22 23:17:12 +0900

    fix to build

--- kernel/src/pci.rs
+++ kernel/src/pci.rs
@@ -1,20 +1,14 @@
-// uint32_t MakeAddress(uint8_t bus, uint8_t device,
-// uint8_t function, uint8_t reg_addr) {
-// auto shl = [](uint32_t x, unsigned int bits) {
-// return x << bits;
-// };
-//
-// return shl(1, 31)  // enable bit
-// | shl(bus, 16)
-// | shl(device, 11)
-// | shl(function, 8)
-// | (reg_addr & 0xfcu);
-// }
-
 use core::ops::Shl;
+
+const CONFIG_ADDRESS: u16 = 0x0cf8;
+
+const CONFIG_DATA: u16 = 0x0cfc;
 
 /// make a 32-bit unsigned integer for CONFIG_ADDRESS
 fn make_address(bus: u8, device: u8, function: u8, reg_addr: u8) -> u32 {
-    let enabled_bit = 1.shl(32);
-    enabled_bit | bus.shl(16) | device.shl(11) | function.shl(8) | (reg_addr & 0xfc)
+    fn shl(x: u8, bits: usize) -> u32 {
+        (x << bits) as u32
+    }
+    let enabled_bit: u32 = (1 as u32) << 32;
+    enabled_bit | shl(bus, 16) | shl(device, 11) | shl(function, 8) | (reg_addr & 0xfc) as u32
 }
