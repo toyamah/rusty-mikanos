0fc1f31bf0b9aca94763079edab2ee56f6b7897a
Haruyoshi Toyama <duane0728@gmail.com>
2021-06-06 06:19:40 +0900

    Update run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,12 +2,19 @@
 
 #set -eu
 set -e
+
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
+cd "$script_dir"
+source ~/osbook/devenv/buildenv.sh
+export RUSTFLAGS="-C link-arg=$LDFLAGS"
+cargo build
+
 cd ~/edk2
+unlink MikanLoaderPkg
+ln -s "$script_dir"/MikanLoaderPkg .
 source edksetup.sh
 build
-
 $HOME/osbook/devenv/run_qemu.sh \
   ./Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi \
   "$script_dir"/kernel.elf
