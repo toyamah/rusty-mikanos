7d2c9aae641917c2dac7a9db7957160dfe027a34
Haruyoshi Toyama <duane0728@gmail.com>
2021-06-21 02:10:06 +0900

    Enable to build the official image

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,22 +2,66 @@
 
 #set -eu
 set -e
-
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-cd "$script_dir"
+build_and_run() {
+  cd "$script_dir"
+
+  # this allows rustc find -lc and -lc++ libraries which are defined in x86~~-elf.json file
+  # see https://doc.rust-lang.org/cargo/reference/config.html#buildrustflags
+  export RUSTFLAGS="-C link-arg=$LDFLAGS"
+  cargo build
+
+  cd ~/edk2
+  unlink MikanLoaderPkg
+  ln -s "$script_dir"/MikanLoaderPkg .
+  source edksetup.sh
+  build
+  $HOME/osbook/devenv/run_qemu.sh \
+    ./Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi \
+    "$script_dir"/kernel.elf
+}
+
+build_and_run_official() {
+  official_dir="$script_dir"/official
+
+  cd "$official_dir"/kernel
+  make
+
+  cd ~/edk2
+  unlink MikanLoaderPkg
+  ln -s "$official_dir"/MikanLoaderPkg .
+  source edksetup.sh
+  build
+  $HOME/osbook/devenv/run_qemu.sh \
+    ./Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi \
+    "$official_dir"/kernel/kernel.elf
+}
+
+parse_params() {
+  official=0
+
+  while :; do
+    case "${1-}" in
+    -v | --verbose) set -x ;;
+    -o | --official) official=1 ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  return 0
+}
+
+parse_params "$@"
+
 source ~/osbook/devenv/buildenv.sh
 
-# this allows rustc find -lc and -lc++ libraries which are defined in x86~~-elf.json file
-# see https://doc.rust-lang.org/cargo/reference/config.html#buildrustflags
-export RUSTFLAGS="-C link-arg=$LDFLAGS"
-cargo build
-
-cd ~/edk2
-unlink MikanLoaderPkg
-ln -s "$script_dir"/MikanLoaderPkg .
-source edksetup.sh
-build
-$HOME/osbook/devenv/run_qemu.sh \
-  ./Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi \
-  "$script_dir"/kernel.elf
+if [ $official -eq 1 ]; then
+  build_and_run_official
+else
+  build_and_run
+fi
