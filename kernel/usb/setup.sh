24c99aa7069ff469550f2238e2ff64b0da802f65
Haruyoshi Toyama <duane0728@gmail.com>
2022-01-05 22:55:17 +0900

    Fix dir where mkdir is executed

--- kernel/usb/setup.sh
+++ kernel/usb/setup.sh
@@ -3,10 +3,12 @@
 set -eu
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-cd "$script_dir"/../../official/kernel
-files=$(\find . -maxdepth 3 -name "*.cpp" -o -name "*.hpp" -o -name "*.c"  | sed -e 's/\.\///g')
+cd $script_dir
 mkdir -p usb/classdriver
 mkdir -p usb/xhci
+
+cd "$script_dir"/../../official/kernel
+files=$(\find . -maxdepth 3 -name "*.cpp" -o -name "*.hpp" -o -name "*.c"  | sed -e 's/\.\///g')
 
 parse_params() {
   clear=0
