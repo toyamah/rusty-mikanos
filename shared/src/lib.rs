1a261cee93756b578ec0abd04d1419351010a8de
Haruyoshi Toyama <duane0728@gmail.com>
2022-01-09 22:57:39 +0900

    Add memory related structs

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use core::ffi::c_void;
+
 #[derive(Eq, PartialEq)]
 #[repr(C)]
 pub enum PixelFormat {
@@ -18,6 +20,56 @@ pub struct FrameBufferConfig {
     pub pixel_format: PixelFormat,
 }
 
+#[warn(dead_code)]
+#[repr(C)]
+#[derive(Debug)]
+pub struct MemoryMap {
+    pub buffer_size: u64,
+    pub buffer: *mut c_void,
+    pub map_size: u64,
+    pub map_key: u64,
+    pub descriptor_size: u64,
+    pub descriptor_version: u32,
+}
+
+#[derive(Eq, PartialEq)]
+#[warn(dead_code)]
+#[repr(C)]
+pub struct MemoryDescriptor {
+    pub type_: MemoryType,
+    pub physical_start: usize,
+    pub virtual_start: usize,
+    pub number_of_pages: u64,
+    pub attribute: u64,
+}
+
+// not adding it to _dummy function to ignore generating to shared_header.h
+#[warn(dead_code)]
+#[derive(Eq, PartialEq, Debug)]
+#[repr(C)]
+pub enum MemoryType {
+    KEfiReservedMemoryType,
+    KEfiLoaderCode,
+    KEfiLoaderData,
+    KEfiBootServicesCode,
+    KEfiBootServicesData,
+    KEfiRuntimeServicesCode,
+    KEfiRuntimeServicesData,
+    KEfiConventionalMemory,
+    KEfiUnusableMemory,
+    KEfiACPIReclaimMemory,
+    KEfiACPIMemoryNVS,
+    KEfiMemoryMappedIO,
+    KEfiMemoryMappedIOPortSpace,
+    KEfiPalCode,
+    KEfiPersistentMemory,
+    KEfiMaxMemoryType,
+}
+
 // doesn't generate code without this method...
 #[no_mangle]
-pub unsafe extern "C" fn _dummy(_: *const FrameBufferConfig) {}
+pub unsafe extern "C" fn _dummy(
+    _: *const FrameBufferConfig,
+    _: MemoryMap,
+    _: MemoryDescriptor,
+) {}
