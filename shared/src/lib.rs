d68255b68ff79cfece50c3c9666d0fd921e8a588
Haruyoshi Toyama <duane0728@gmail.com>
2022-01-10 01:05:13 +0900

    Display MemoryMap

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -20,6 +20,10 @@ pub struct FrameBufferConfig {
     pub pixel_format: PixelFormat,
 }
 
+/// To generate unsigned long long type, each value should be defined as c_ulonglong.
+/// However, c_longlong is in the std library which cannot be used here.
+/// To solve this, I decided to keep MemoryMap from generating to shared_header.h and left the original MemoryMap type in Main.c.
+/// https://github.com/eqrion/cbindgen/blob/master/docs.md#libc-types
 #[warn(dead_code)]
 #[repr(C)]
 #[derive(Debug)]
@@ -66,10 +70,34 @@ pub enum MemoryType {
     KEfiMaxMemoryType,
 }
 
+impl MemoryType {
+    pub fn to_i32(&self) -> i32 {
+        return match self {
+            MemoryType::KEfiReservedMemoryType => 0,
+            MemoryType::KEfiLoaderCode => 1,
+            MemoryType::KEfiLoaderData => 2,
+            MemoryType::KEfiBootServicesCode => 3,
+            MemoryType::KEfiBootServicesData => 4,
+            MemoryType::KEfiRuntimeServicesCode => 5,
+            MemoryType::KEfiRuntimeServicesData => 6,
+            MemoryType::KEfiConventionalMemory => 7,
+            MemoryType::KEfiUnusableMemory => 8,
+            MemoryType::KEfiACPIReclaimMemory => 9,
+            MemoryType::KEfiACPIMemoryNVS => 10,
+            MemoryType::KEfiMemoryMappedIO => 11,
+            MemoryType::KEfiMemoryMappedIOPortSpace => 12,
+            MemoryType::KEfiPalCode => 13,
+            MemoryType::KEfiPersistentMemory => 14,
+            MemoryType::KEfiMaxMemoryType => 15,
+        };
+    }
+}
+
 // doesn't generate code without this method...
 #[no_mangle]
 pub unsafe extern "C" fn _dummy(
     _: *const FrameBufferConfig,
-    _: MemoryMap,
+    // _: MemoryMap,
     _: MemoryDescriptor,
-) {}
+) {
+}
