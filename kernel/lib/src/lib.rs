f5cf43b74a46773f3dfca782b2c4b5eb85d2897c
Haruyoshi Toyama <duane0728@gmail.com>
2022-01-25 21:31:20 +0900

    Delete test created by cargo

--- kernel/lib/src/lib.rs
+++ kernel/lib/src/lib.rs
@@ -18,11 +18,3 @@ pub mod queue;
 pub mod segment;
 pub mod window;
 mod x86_descriptor;
-
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
