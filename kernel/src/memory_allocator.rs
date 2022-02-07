04ff3ad60cbcff74b0750f3066efaac38ae8427c
Haruyoshi Toyama <duane0728@gmail.com>
2022-02-07 20:43:20 +0900

    Consider memory alignment

--- kernel/src/memory_allocator.rs
+++ kernel/src/memory_allocator.rs
@@ -6,11 +6,13 @@ use log::error;
 
 pub struct MemoryAllocator;
 
+// I'm not sure that this implementation is correct especially about memory alignment...
+// But seems like it works.
 unsafe impl GlobalAlloc for MemoryAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let num_frames = (layout.size() + BYTES_PER_FRAME - 1) / BYTES_PER_FRAME;
+        let num_frames = (layout.size() + layout.align() + BYTES_PER_FRAME - 1) / BYTES_PER_FRAME;
         match memory_manager().allocate(num_frames) {
-            Ok(frame) => (frame.id() * BYTES_PER_FRAME) as *mut u8,
+            Ok(frame) => (frame.id() * BYTES_PER_FRAME + layout.align()) as *mut u8,
             Err(e) => {
                 error!("an error occurs when allocating {:?}: {}", layout, e);
                 null_mut()
@@ -20,7 +22,7 @@ unsafe impl GlobalAlloc for MemoryAllocator {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         let start_frame = FrameID::new(ptr as usize / BYTES_PER_FRAME);
-        let num_frames = (layout.size() + BYTES_PER_FRAME - 1) / BYTES_PER_FRAME;
+        let num_frames = (layout.size() + layout.align() + BYTES_PER_FRAME - 1) / BYTES_PER_FRAME;
         memory_manager().free(start_frame, num_frames).unwrap();
     }
 }
