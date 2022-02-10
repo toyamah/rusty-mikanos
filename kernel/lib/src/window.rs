d8e23ceac415485e7757843104ce05551fc92ce1
Haruyoshi Toyama <duane0728@gmail.com>
2022-02-10 19:55:49 +0900

    Remove RefCell

--- kernel/lib/src/window.rs
+++ kernel/lib/src/window.rs
@@ -1,15 +1,14 @@
 use crate::frame_buffer::FrameBuffer;
 use crate::graphics::{PixelColor, PixelWriter, Rectangle, Vector2D};
 use alloc::vec::Vec;
-use core::cell::RefCell;
 use shared::{FrameBufferConfig, PixelFormat};
 
 pub struct Window {
     width: usize,
     height: usize,
     //TODO: try to define without RefCel
-    data: RefCell<Vec<Vec<PixelColor>>>,
-    shadow_buffer: RefCell<FrameBuffer>,
+    data: Vec<Vec<PixelColor>>,
+    shadow_buffer: FrameBuffer,
     transparent_color: Option<PixelColor>,
 }
 
@@ -21,9 +20,8 @@ impl Window {
         let data: Vec<Vec<_>> = (0..height)
             .map(|_| (0..width).map(|_| PixelColor::new(0, 0, 0)).collect())
             .collect();
-        let data = RefCell::new(data);
         let config = FrameBufferConfig::new(width as u32, height as u32, 0, shadow_format);
-        let shadow_buffer = RefCell::new(FrameBuffer::new(config));
+        let shadow_buffer = FrameBuffer::new(config);
         Self {
             width,
             height,
@@ -33,10 +31,11 @@ impl Window {
         }
     }
 
+    //TODO: change self to mutable reference if possible
     pub fn draw_to(&self, dst: &mut FrameBuffer, position: Vector2D<i32>) {
         match self.transparent_color {
             None => {
-                dst.copy(position, &self.shadow_buffer.borrow());
+                dst.copy(position, &self.shadow_buffer);
             }
             Some(transparent) => self.on_each_pixel(move |x, y| {
                 let color = self.at(x, y);
@@ -49,7 +48,7 @@ impl Window {
     }
 
     pub fn move_(&mut self, pos: Vector2D<i32>, src: &Rectangle<i32>) {
-        self.shadow_buffer.borrow_mut().move_(pos, src)
+        self.shadow_buffer.move_(pos, src)
     }
 
     pub fn set_transparent_color(&mut self, c: PixelColor) {
@@ -73,14 +72,14 @@ impl Window {
     }
 
     fn at(&self, x: usize, y: usize) -> PixelColor {
-        self.data.borrow()[y][x]
+        self.data[y][x]
     }
 }
 
 impl PixelWriter for Window {
     fn write(&mut self, x: i32, y: i32, color: &PixelColor) {
-        self.data.borrow_mut()[y as usize][x as usize] = *color;
-        self.shadow_buffer.borrow_mut().writer().write(x, y, color);
+        self.data[y as usize][x as usize] = *color;
+        self.shadow_buffer.writer().write(x, y, color);
     }
 
     fn width(&self) -> i32 {
