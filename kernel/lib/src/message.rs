8910c112424ebb1ee975b8420c98f69e1f2df0da
Haruyoshi Toyama <duane0728@gmail.com>
2022-03-06 00:52:07 +0900

    Define message arg as enum

--- kernel/lib/src/message.rs
+++ kernel/lib/src/message.rs
@@ -1,59 +1,24 @@
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Message {
     pub m_type: MessageType,
-    pub arg: Arg,
 }
 
 impl Message {
-    pub const fn new(m_type: MessageType, arg: Arg) -> Message {
-        Self { m_type, arg }
+    pub const fn new(m_type: MessageType) -> Message {
+        Self { m_type }
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum MessageType {
     InterruptXhci,
-    TimerTimeout,
-    KeyPush,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub union Arg {
-    pub timer: TimerMessage,
-    pub keyboard: Keyboard,
-    none: (),
-}
-
-impl Arg {
-    pub const NONE: Arg = Arg { none: () };
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct TimerMessage {
-    pub timeout: u64,
-    pub value: i32,
-}
-
-impl TimerMessage {
-    pub fn new(timeout: u64, value: i32) -> TimerMessage {
-        Self { timeout, value }
-    }
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct Keyboard {
-    pub modifier: u8,
-    pub keycode: u8,
-    pub ascii: char,
-}
-
-impl Keyboard {
-    pub fn new(modifier: u8, keycode: u8, ascii: char) -> Keyboard {
-        Self {
-            modifier,
-            keycode,
-            ascii,
-        }
-    }
+    TimerTimeout {
+        timeout: u64,
+        value: i32,
+    },
+    KeyPush {
+        modifier: u8,
+        keycode: u8,
+        ascii: char,
+    },
 }
