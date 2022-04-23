88988855b9eb49f567ef1b578746d9d3cff01bda
Haruyoshi Toyama <duane0728@gmail.com>
2022-04-23 23:46:42 +0900

    Implement segment

--- kernel/lib/src/segment.rs
+++ kernel/lib/src/segment.rs
@@ -4,13 +4,20 @@ use bit_field::BitField;
 pub const KERNEL_CS: u16 = 1 << 3;
 pub const KERNEL_SS: u16 = 2 << 3;
 const KERNEL_DS: u16 = 0;
+const K_TSS: u16 = 5 << 3;
 
 pub mod global {
     use super::{SegmentDescriptor, KERNEL_CS, KERNEL_DS, KERNEL_SS};
-    use crate::asm::global::{load_gdt, set_csss, set_ds_all};
-    use crate::x86_descriptor::SegmentDescriptorType;
+    use crate::asm::global::{load_gdt, load_tr, set_csss, set_ds_all};
+    use crate::memory_manager::global::memory_manager;
+    use crate::segment::K_TSS;
+    use crate::x86_descriptor::SystemDescriptorType::TSSAvailable;
+    use crate::x86_descriptor::{SegmentDescriptorType, SystemDescriptorType};
+    use core::mem;
+    use log::info;
 
-    static mut GDT: [SegmentDescriptor; 5] = [SegmentDescriptor::new(); 5];
+    static mut GDT: [SegmentDescriptor; 7] = [SegmentDescriptor::new(); 7];
+    static mut TSS: [u32; 26] = [0; 26];
 
     pub fn initialize() {
         set_up_segment();
@@ -25,10 +32,36 @@ pub mod global {
             GDT[3].set_code_segment(SegmentDescriptorType::ExecuteRead, 3, 0, 0xfffff);
             GDT[4].set_code_segment(SegmentDescriptorType::ReadWrite, 3, 0, 0xfffff);
             load_gdt(
-                (core::mem::size_of_val(&GDT) - 1) as u16,
+                core::mem::size_of_val(&GDT) as u16 - 1,
                 &GDT[0] as *const _ as u64,
             );
         }
+    }
+
+    pub fn initialize_tss() {
+        let k_rsp0frames = 8;
+
+        let stack0 = memory_manager()
+            .allocate(k_rsp0frames)
+            .expect("failed to allocate rsp0");
+        let rsp0 = stack0.frame() as u64 + k_rsp0frames * 4096;
+        unsafe {
+            TSS[1] = (rsp0 & 0xffffffff) as u32;
+            TSS[2] = (rsp0 >> 32) as u32;
+        }
+
+        let tss_addr = &tss[0] as usize;
+        unsafe {
+            let i = (K_TSS >> 3) as usize;
+            GDT[i].set_system_segment2(
+                SegmentDescriptorType::TSSAvailable,
+                0,
+                (tss_addr & 0xffff_ffff) as u32,
+                (mem::size_of_val(&TSS) - 1) as u32,
+            );
+            GDT[i + 1] = SegmentDescriptor(0);
+        }
+        load_tr(K_TSS);
     }
 }
 
@@ -77,6 +110,18 @@ impl SegmentDescriptor {
         self.set_code_segment(type_, descriptor_privilege_level, base, limit);
         self.set_long_mode(0);
         self.set_default_operation_size(1); // 32-bit stack segment
+    }
+
+    pub fn set_system_segment2(
+        &mut self,
+        type_: SegmentDescriptorType,
+        descriptor_privilege_level: u32,
+        base: u32,
+        limit: u32,
+    ) {
+        self.set_code_segment(type_, descriptor_privilege_level, base, limit);
+        self.set_system_segment(0);
+        self.set_long_mode(0);
     }
 
     // uint64_t limit_low : 16; 0..16
