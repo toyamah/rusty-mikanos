bc9500654cf1ae2d24673401aa72f662c031f6f9
Haruyoshi Toyama <duane0728@gmail.com>
2022-05-02 20:37:43 +0900

    Move functions

--- apps/shared_lib/src/lib.rs
+++ apps/shared_lib/src/lib.rs
@@ -1,21 +1,21 @@
 #![no_std]
 
 use crate::byte_buffer::ByteBuffer;
-use crate::newlib_support::{write, SyscallResult};
+use crate::newlib_support::write;
 use crate::rust_official::cchar::c_char;
+use crate::syscall::SyscallLogString;
 use core::ffi::c_void;
 use core::fmt;
 
 mod byte_buffer;
 pub mod newlib_support;
 pub mod rust_official;
+mod syscall;
 
 extern "C" {
     pub fn strlen(cs: *const c_char) -> usize;
     pub fn atol(s: *const c_char) -> i64;
     // pub fn strcmp(a: *const c_char, b: *const c_char) -> i32;
-
-    pub(crate) fn SyscallLogString(level: i64, s: *const c_char) -> SyscallResult;
 }
 
 pub fn print(s: &str) {
