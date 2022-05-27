7cab796a71ff0e4b3dea63c2ad521fcc95f02e35
Haruyoshi Toyama <duane0728@gmail.com>
2022-05-28 01:27:10 +0900

    Create cp app

---
+++ apps/cp/src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+#![feature(format_args_nl)]
+use core::arch::asm;
+use core::panic::PanicInfo;
+use shared_lib::newlib_support::exit;
+use shared_lib::rust_official::cchar::c_char;
+
+#[no_mangle]
+pub extern "C" fn main(_argc: i32, _argv: *const *const c_char) {
+    exit(1)
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe { asm!("hlt") }
+    }
+}
