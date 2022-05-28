6ba609c731bc9537d1dcf2b7480102d2920962a0
Haruyoshi Toyama <duane0728@gmail.com>
2022-05-29 00:41:56 +0900

    Implement cp

--- apps/cp/src/main.rs
+++ apps/cp/src/main.rs
@@ -3,12 +3,47 @@
 #![feature(format_args_nl)]
 use core::arch::asm;
 use core::panic::PanicInfo;
+use shared_lib::args::Args;
+use shared_lib::file::{open_file, read_file2, write_file, OpenMode};
 use shared_lib::newlib_support::exit;
+use shared_lib::println;
 use shared_lib::rust_official::cchar::c_char;
 
 #[no_mangle]
-pub extern "C" fn main(_argc: i32, _argv: *const *const c_char) {
-    exit(1)
+pub extern "C" fn main(argc: i32, argv: *const *const c_char) {
+    let args = Args::new(argc, argv);
+    if args.len() < 3 {
+        println!("Usage: {} <src> <dest>", args.get(0));
+        exit(1);
+    }
+
+    let fp_src = open_file(args.get(1), OpenMode::R);
+    if fp_src.is_null() {
+        println!("failed to open for read: {}", args.get(1));
+        exit(1);
+    }
+
+    let fp_dest = open_file(args.get(2), OpenMode::W);
+    if fp_dest.is_null() {
+        println!("failed to open for write: {}", args.get(2));
+        exit(1);
+    }
+
+    let mut buf = [0_u8; 256];
+    loop {
+        let bytes = read_file2(fp_src, &mut buf);
+        if bytes <= 0 {
+            break;
+        }
+        let written = write_file(fp_dest, &buf[bytes..]);
+        println!("bytes {}, written = {}", bytes, written);
+        if bytes != written {
+            println!("failed to write to {}", args.get(2));
+            exit(1);
+        }
+    }
+
+    exit(0)
 }
 
 #[panic_handler]
